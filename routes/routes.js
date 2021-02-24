6413288b231e23ae9b55d7e6e22bd7e5c426aa4f
oliver <soliz78120@gmail.com>
2021-02-24 10:21:43 -0400

    test1 primera clase de apirest

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,12 @@ import IndexController from "../controller/indexController.js";
 var router = express.Router();
 var indexControler = new IndexController();
 /* GET home page. */
+
 router.get("/", indexControler.index);
 
+router.post("/ecuation", indexControler.ecuationSolve);
+router.post("/ecuationimg", indexControler.ecuationSolveImg);
+router.post("/plus", indexControler.plus);
 export default router;
+
+ 
